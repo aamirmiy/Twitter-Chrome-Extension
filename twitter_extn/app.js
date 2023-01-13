71f7ed764ee05f42e2e3ec58cb4c11d07d391a79
Aamir Miyajiwala <aamir.miyajiwala@gmail.com>
2023-01-13 01:50:13 -0800

    Deployment test

--- twitter_extn/app.js
+++ twitter_extn/app.js
@@ -38,5 +38,6 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
-
+const port= process.env.PORT || 3000;
+app.listen(port, () => console.log("Server is running on port",port));
 module.exports = app;
